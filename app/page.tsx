25cdc0283f4e0107a1d8f6316a75ecb22ccd4dec
César Fontalvo <cafc2030@gmail.com>
2026-03-01 17:05:31 -0500

    feat: adds links comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { CLIENTS, CONTACTS_LINKS } from "@/constants";
 import BrandsMarquee from "../components/BrandsMarquee";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -30,18 +31,18 @@ export default function Home() {
 
             <div className="ctaRow">
               {/* ideal: que abra WhatsApp */}
-              <a
+              <Link
                 className="btnPrimary"
                 href="https://wa.me/573177788997?text=Hola,%20quisiera%20cotizar%20servicios/repuestos."
                 target="_blank"
                 rel="noreferrer"
               >
                 Cotiza por WhatsApp
-              </a>
+              </Link>
 
-              <a className="btnGhost" href="#servicios">
+              <Link className="btnGhost" href="#servicios">
                 Ver servicios
-              </a>
+              </Link>
             </div>
           </div>
 
@@ -438,14 +439,14 @@ export default function Home() {
             </div>
 
             <div className="centerRow">
-              <a
+              <Link
                 className="btnDark"
                 href={CONTACTS_LINKS.whatsapp.url}
                 target="_blank"
                 rel="noreferrer"
               >
                 Escríbenos por WhatsApp
-              </a>
+              </Link>
             </div>
           </div>
         </section>
