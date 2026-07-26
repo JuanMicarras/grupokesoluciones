c2e4b11fb1fff12eb5f60076bf312bf06c35b3f9
Juan Miguel Carrasquilla <jmcarrasquilla@uninorte.edu.co>
2026-07-26 18:02:08 -0500

    Cambios para Vercel
    Ya la pagina no va a ser estatica

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: { unoptimized: true },
   pageExtensions: ["ts", "tsx", "mdx"],
   trailingSlash: true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
